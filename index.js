8b5da30e851c385f9f2edc0dd792f5d78e766286
vijays376 <vijayschouhan376@gmail.com>
2025-03-30 21:51:25 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('✅ MongoDB Connected'))
     .catch(err => console.log('❌ MongoDB Connection Error:', err));
 
